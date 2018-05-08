6a729c5d103293f825cf06d5c523c4ab6c654036
Alexander Akbashev <mr.akbashev@gmail.com>
2018-05-08 10:45:26 +0200

    Consider as stuck iteams stayed in queue for 90 minutes
    ... it was initially intended :)

--- scripts/purge_stuck_items_from_queue.groovy
+++ scripts/purge_stuck_items_from_queue.groovy
@@ -4,7 +4,7 @@ def q = Jenkins.instance.queue
 long now = new Date().getTime()
 
 q.items.findAll { 
-        (now - it.getInQueueSince())/6000 > 90
+        (now - it.getInQueueSince())/60000 > 90
     }.each { 
         q.cancel(it.task) 
     }
